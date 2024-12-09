d69e9de00540f355a02ab0012a4bde981e4f0a05
Barmington Chuku <barmingtonk@gmail.com>
2024-12-09 16:06:05 +0000

    added

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,15 +1,16 @@
-import auth from "../app/middleware";
-import { LoginButton } from "./LoginButton";
-import { LogoutButton } from "./LogoutButton";
+import auth from '../app/middleware';
+import { LoginButton } from './LoginButton';
+import { LogoutButton } from './LogoutButton';
 
 export async function UserInfo() {
   const session = await auth();
+  console.log(session);
 
   return (
     <div>
       {session ? (
         <div>
-          {session.user.name}{" "}
+          {session.user.name}{' '}
           <span className="text-xs text-zinc-400 mr-3">#{session.user.id}</span>
           <LogoutButton />
         </div>
